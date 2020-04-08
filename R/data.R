416fa22b2b80b6192ee636033c8d5c4f10355f10
KelseyChetnik <kelsey.chetnik@gmail.com>
2020-04-08 15:47:04 -0400

    MetaClean first commit

---
+++ R/data.R
@@ -0,0 +1,21 @@
+#' Example Peak Quality Metrics Data Frame for Development Dataset.
+#'
+#' Data frame with peaks quality metrics and labels for all of the 500 EICs in the example development dataset.
+#'
+#' @format A data frame with 14 variables (EIC Number, the 12 peak quality metrics, and Class Labels): \code{EICNo},
+#' \code{ApexBoundaryRatio_mean}, \code{ElutionShift_mean}, \code{FWHM2Base_mean}, \code{Jaggedness_mean}, \code{Modelaity_mean},
+#' \code{RetentionTimeCorrelation_mean}, \code{Symmetry_mean}, \code{GaussianSimilarity_mean}, \code{PeakSignificance_mean},
+#' \code{Sharpness_mean}, \code{TPASR_mean}, \code{ZigZag_mean}, and \code{Class}.
+#'
+"pqMetrics_development"
+
+#' Example Peak Quality Metrics Data Frame for Test Dataset.
+#'
+#' Data frame with peaks quality metrics and labels for all of the 500 EICs in the example test dataset.
+#'
+#' @format A data frame with 14 variables (EIC Number, the 12 peak quality metrics, and Class Labels): \code{EICNo},
+#' \code{ApexBoundaryRatio_mean}, \code{ElutionShift_mean}, \code{FWHM2Base_mean}, \code{Jaggedness_mean}, \code{Modelaity_mean},
+#' \code{RetentionTimeCorrelation_mean}, \code{Symmetry_mean}, \code{GaussianSimilarity_mean}, \code{PeakSignificance_mean},
+#' \code{Sharpness_mean}, \code{TPASR_mean}, \code{ZigZag_mean}, and \code{Class}.
+#'
+"pqMetrics_test"
